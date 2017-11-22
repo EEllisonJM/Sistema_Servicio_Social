4cfdc3a53e59639fc74fa82c8116e59a069a449f
EEllisonJM <ellison_jm@hotmail.com>
2017-11-22 08:23:40 -0600

    Mostrar documento en pantalla

--- Sistema_Servicio_Social/Interfaz.xaml.cs
+++ Sistema_Servicio_Social/Interfaz.xaml.cs
@@ -1,24 +1,17 @@
 ﻿using Microsoft.Win32;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using System.IO;
+using System.Windows.Xps.Packaging;
+using Microsoft.Office.Interop.Word;
+using Word = Microsoft.Office.Interop.Word;
 
 namespace Sistema_Servicio_Social
 {
     /// <summary>
     /// Lógica de interacción para Interfaz.xaml
     /// </summary>
-    public partial class Interfaz : Window
+    public partial class Interfaz : System.Windows.Window
     {
         public Interfaz()
         {
@@ -64,5 +57,89 @@ namespace Sistema_Servicio_Social
                }
             
         }
-     }
+        //---
+
+        /// <summary>
+        ///  Select Word file 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSelectWord_Click(object sender, RoutedEventArgs e)
+        {
+            // Initialize an OpenFileDialog
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            // Set filter and RestoreDirectory
+            openFileDialog.RestoreDirectory = true;
+            openFileDialog.Filter = "Word documents(*.doc;*.docx)|*.doc;*.docx";
+            bool? result = openFileDialog.ShowDialog();
+            if (result == true)
+            {
+                if (openFileDialog.FileName.Length > 0)
+                {
+                    txbSelectedWordFile.Text = openFileDialog.FileName;
+                }
+            }
+        }
+
+        /// <summary>
+        ///  Convert the word document to xps document
+        /// </summary>
+        /// <param name="wordFilename">Word document Path</param>
+        /// <param name="xpsFilename">Xps document Path</param>
+        /// <returns></returns>
+        private XpsDocument ConvertWordToXps(string wordFilename, string xpsFilename)
+        {
+            // Create a WordApplication and host word document
+            Word.Application wordApp = new Microsoft.Office.Interop.Word.Application();
+            try
+            {
+                wordApp.Documents.Open(wordFilename);
+
+                // To Invisible the word document
+                wordApp.Application.Visible = false;
+
+                // Minimize the opened word document
+                wordApp.WindowState = WdWindowState.wdWindowStateMinimize;
+                Document doc = wordApp.ActiveDocument;
+                doc.SaveAs(xpsFilename, WdSaveFormat.wdFormatXPS);
+
+                XpsDocument xpsDocument = new XpsDocument(xpsFilename, FileAccess.Read);
+                return xpsDocument;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error occurs, The error message is  " + ex.ToString());
+                return null;
+            }
+            finally
+            {
+                wordApp.Documents.Close();
+                ((_Application)wordApp).Quit(WdSaveOptions.wdDoNotSaveChanges);
+            }
+        }
+        /// <summary>
+        ///  View Word Document in WPF DocumentView Control
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnViewDoc_Click(object sender, RoutedEventArgs e)
+        {
+            string wordDocument = txbSelectedWordFile.Text;
+            if (string.IsNullOrEmpty(wordDocument) || !File.Exists(wordDocument))
+            {
+                MessageBox.Show("The file is invalid. Please select an existing file again.");
+            }
+            else
+            {
+                string convertedXpsDoc = string.Concat(System.IO.Path.GetTempPath(), "\\", Guid.NewGuid().ToString(), ".xps");
+                XpsDocument xpsDocument = ConvertWordToXps(wordDocument, convertedXpsDoc);
+                if (xpsDocument == null)
+                {
+                    return;
+                }
+
+                documentviewWord.Document = xpsDocument.GetFixedDocumentSequence();
+            }
+        }
+    }
 }
--- Sistema_Servicio_Social/MainWindow.xaml.cs
+++ Sistema_Servicio_Social/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -29,6 +30,7 @@ namespace Sistema_Servicio_Social
         {
             Main.Content = new Login();
         }
+
         
     }
 }
