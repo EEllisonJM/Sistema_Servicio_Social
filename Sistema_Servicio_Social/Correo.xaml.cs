be1a436fc5a4a1cb2981fd1d2edc2df0f66bf802
Richard <Green-Ultimate@hotmail.com>
2017-11-29 07:57:46 -0600

    Método para enviar el correo
    Nota: la vista del correo sigue vacía

---
+++ Sistema_Servicio_Social/Correo.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Outlook = Microsoft.Office.Interop.Outlook;
+
+namespace CorAdjunto
+{
+     class Program
+     {
+          public void EnviarCorreo(string ruta, string nombre, string asunto, string mensaje)
+          {
+               try
+               {
+                    // Create the Outlook application by using inline initialization.
+                    Outlook.Application oApp = new Outlook.Application();
+
+                    //Create the new message by using the simplest approach.
+                    Outlook.MailItem oMsg = (Outlook.MailItem)oApp.CreateItem(Outlook.OlItemType.olMailItem);
+
+                    //Add a recipient.
+                    // TODO: Change the following recipient where appropriate.
+                    Outlook.Recipient oRecip = (Outlook.Recipient)oMsg.Recipients.Add("gike@tinoza.org");
+                    oRecip.Resolve();
+
+                    //Set the basic properties.
+                    oMsg.Subject = asunto;
+                    oMsg.Body = mensaje;
+
+                    //Add an attachment.
+                    // TODO: change file path where appropriate
+                    String sSource = ruta;//"C:\\Users\\RICHARD\\Desktop\\ito\\CARRERAS\\ELECTRICA\\Objetivo IELE-2010-209.pdf";
+                    String sDisplayName = nombre;//"MyFirstAttachment.pdf";
+                    int iPosition = (int)oMsg.Body.Length + 1;
+                    int iAttachType = (int)Outlook.OlAttachmentType.olByValue;
+                    Outlook.Attachment oAttach = oMsg.Attachments.Add(sSource, iAttachType, iPosition, sDisplayName);
+
+                    // If you want to, display the message.
+                    // oMsg.Display(true);  //modal
+
+                    //Send the message.
+                    oMsg.Save();
+                    oMsg.Send();
+
+                    //Explicitly release objects.
+                    oRecip = null;
+                    oAttach = null;
+                    oMsg = null;
+                    oApp = null;
+               }
+
+               // Simple error handler.
+               catch (Exception e)
+               {
+                    Console.WriteLine("{0} Exception caught: ", e);
+               }
+
+               //Default return value.
+               //return 0;
+          }
+     }
+}
