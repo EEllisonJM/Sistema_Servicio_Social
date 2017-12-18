da4015ef130b93d783a6c3578ee1efadc510e39c
EEllisonJM <ellison_jm@hotmail.com>
2017-12-18 10:11:08 -0600

    Revert "Correccion cartaPresentacion-Alumno"
    This reverts commit dea5315949e0e9a1dedc477124913795bb0c7fe2.

--- Sistema_Servicio_Social/ConexionMySQL.cs
+++ Sistema_Servicio_Social/ConexionMySQL.cs
@@ -2,8 +2,6 @@
 using System;
 using System.IO;
 using System.Text;
-using System.Windows.Forms;
-
 namespace Sistema_Servicio_Social
 {
     class ConexionMySQL
@@ -15,7 +13,7 @@ namespace Sistema_Servicio_Social
             foreach (string line in File.ReadLines(@"" + ruta))
             {
                 String[] values = line.Split(',');
-                if (values[0] != "Marca temporal")
+                if (values[0] != "\"Marca temporal\"")
                 {
                     //Eliminar las " de los campos y cambiar los datos a Mayúsculas
                     for (int i = 0; i <= 17; i++)
@@ -32,51 +30,20 @@ namespace Sistema_Servicio_Social
                         values[6] = "M";
                     }
                     DBConnect db = new DBConnect();
-                    /*======ALUMNO EXISTE===========================================*/
-                    if (db.CountOne(/*Existe alumno? */
+                    if (db.CountOne(//Existe?
                         "Alumno",//Table
                         "numControl", values[2]) == 1)//numControl=values[2]?
                     {
-                        DialogResult result = MessageBox.Show(
-                            "El número de control que intenta guardar ya se encuentra en la base de datos, ¿Desea actualizar sus datos?",
-                            "Número de control",
-                            MessageBoxButtons.YesNoCancel);
-                        if (result == DialogResult.Yes)
-                        {
-                            db.Update(//Actualiza
+                        db.Update(//Actualiza
                             "Alumno",//Tabla
-                            /*SET*/
                             "nombre = '" + values[5] + " " + values[3] + " " + values[4] + "'," +//nombre
                             "carrera = '" + values[7] + "'," +//carrera
                             "sexo = '" + values[6] + "'," +//sexo
                             "e_mail = '" + values[1] + "'," +//e-mail
                             "porcentajeAvance = " + values[8] + "," +//porcentaje de avance
                             "semestre = " + values[9],//Semestre
-                            /*WHERE*/
                             "numControl", values[2]);//Numero de control
-
-                        }
-                        else if (result == DialogResult.No)
-                        {
-                            //No hacer nada
-                        }
-                    }
-                    /*======ALUMNO NO EXISTE===========================================*/
-                    else
-                    {
-                        db.Insert(//Insertar
-                            "Alumno",//Tabla
-                        "(numControl,nombre,carrera,sexo,e_mail,porcentajeAvance,semestre)",//Atributos
-                        "(" +//Valores...
-                        values[2] + "," +//numControl
-                        " '" + values[5] + " " + values[3] + " " + values[4] + "'," +//nombre
-                        " '" + values[7] + "'," +//carrera
-                        " '" + values[6] + "'," +//sexo
-                        " '" + values[1] + "'," +//e-mail
-                        values[8] + "," +//porcentaje de avance
-                        values[9] + ")"//Semestre
-                        );
-
+                        //
                         db.Insert(//Insertar
                             "Carta_Presentacion",//Tabla
                             "(numExpediente,anio,numControl,nombreDependencia,direccionDependencia,programa,jefeDireccion,puestoJefeDireccion,leyenda)",//Atributos
@@ -92,8 +59,56 @@ namespace Sistema_Servicio_Social
                             );
                         numExp++;
                     }
+                    else
+                    {
+                        db.Insert(//Insertar
+                            "Alumno",//Tabla
+                        "(numControl,nombre,carrera,sexo,e_mail,porcentajeAvance,semestre)",//Atributos
+                        "(" +//Valores...
+                        values[2] + "," +//numControl
+                        " '" + values[5] + " " + values[3] + " " + values[4] + "'," +//nombre
+                        " '" + values[7] + "'," +//carrera
+                        " '" + values[6] + "'," +//sexo
+                        " '" + values[1] + "'," +//e-mail
+                        values[8] + "," +//porcentaje de avance
+                        values[9] + ")"//Semestre
+                        );
+                    }
+                    if (db.CountOne(//Existe?
+                        "Carta_Presentacion",//Tabla
+                        "numControl", values[2]) == 1)//numControl=values[2]?
+                    {
+                        db.Update(//Actualiza
+                            "Carta_Presentacion",//Tabla
+                            "nombreDependencia = '" + values[10] + "'," +
+                            "direccionDependencia = '" + values[12] + "'," +
+                            "programa = '" + values[11] + "'," +
+                            "nombreDependencia = '" + values[10] + "'," +
+                            "jefeDireccion= '" + values[13] + " " + values[14] + " " + values[15] + " " + values[16] + "'," +
+                            "leyenda = '" + leyenda + "'" +//"leyenda = 'Soy leyenda' ",
+                            "anio = " + anio + "",//Año
+                            "numControl", "" + values[2] + ""//Donde => numExpediente=numExp
+                            );
+                    }
+                    else
+                    {
+                        db.Insert(//Insertar
+                            "Carta_Presentacion",//Tabla
+                            "(numExpediente,anio,numControl,nombreDependencia,direccionDependencia,programa,jefeDireccion,puestoJefeDireccion,leyenda)",//Atributos
+                            "(" +//Valores...
+                            numExp + "," +//numExpediente
+                            anio + "," +//Año
+                            values[2] + ",'" +//numControl
+                            values[10] + "','" +//nombreDependencia
+                            values[12] + "','" +//direccionDependencia
+                            values[11] + "','" +//programa
+                            values[13] + ' ' + values[14] + ' ' + values[15] + ' ' + values[16] + "','" +//jefeDireccion
+                            values[17] + "','"+leyenda+"')"//puesto y leyenda
+                            );
+                        numExp++;
+                    }
                 }
-            }//Fin foreach          
-        }//Fin metodo leerCSV
+            }
+        }
     }
 }
--- Sistema_Servicio_Social/DBConnect.cs
+++ Sistema_Servicio_Social/DBConnect.cs
@@ -23,8 +23,7 @@ namespace Sistema_Servicio_Social
             database = "Servicio_Social";
             uid = "root";
             port = "3306";
-            //password = "1234";
-            password = "";
+            password = "1234";
             string connectionString;
             connectionString = "PORT=" + port + ";" + "SERVER=" + server + ";" + "DATABASE=" +
             database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
